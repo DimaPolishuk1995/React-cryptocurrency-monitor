e42182cfa4d50bc996e1eea1204b2e54d03c4dd4
DimaPolishuk1995 <suvenir9502@gmail.com>
2018-05-02 13:10:59 +0300

    Add material-ui to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,25 @@
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import * as injectTapEventPlugin from 'react-tap-event-plugin';
 
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/store';
 
+injectTapEventPlugin();
 const store = configureStore();
 
 const app = (
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <MuiThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MuiThemeProvider>
   </Provider>
 );
 
