b007af77149d09d504fe5876ff23486843f17f57
DimaPolishuk1995 <suvenir9502@gmail.com>
2018-05-08 13:02:37 +0300

    Create test saga functions and add basic structure

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,5 +1,12 @@
-import { all } from 'redux-saga/effects';
+import { all } from 'redux-saga/effects'
+import { delay } from 'redux-saga'
 
+function* helloSagas() {
+  yield delay(1000);
+  console.log('Hello Dmitriy, you are use redux-saga')
+}
 export default function* sagas() {
-  yield all([]);
+  yield all([
+    helloSagas()
+  ]);
 }
