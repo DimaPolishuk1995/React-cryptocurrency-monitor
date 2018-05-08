741bc45d28af1fe788e1766329c671377158b382
DimaPolishuk1995 <suvenir9502@gmail.com>
2018-05-08 14:16:52 +0300

    Add material-ui to project

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
         <span>Home page</span>
+        <AppBar
+          title="Title"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
       </div>
     );
   }
